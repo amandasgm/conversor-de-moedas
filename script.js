9b99ed73c3156b8331bcba92744b4b7e7b457ffb
Amanda <amanda.asgm@gmail.com>
2025-05-13 10:35:31 -0300

    configurando formatação e exibição da moeda/resultado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const USD = 4.87
 const EUR = 5.32
-const AR = 205
+const GBP = 4.20
 
 // Obtendo elementos do formulario
 const form = document.querySelector("form")
@@ -10,7 +10,7 @@ const footer = document.querySelector("main footer")
 const span = document.querySelector("footer span")
 const result = document.querySelector("footer h1")
 
-// Manipulando o input amount para receber somente numeros
+// Manipulando o input amount para receber somente numeros | daria no mesmo se eu deixasse o input do tipo number
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "") 
@@ -27,8 +27,8 @@ form.addEventListener("submit", (e) => {
     case "EUR":
       convertCurrency(amount.value, EUR, "€")
       break
-    case "AR":
-      convertCurrency(amount.value, AR, "$")
+    case "GBP":
+      convertCurrency(amount.value, GBP, "$")
       break
   }
 })
@@ -36,14 +36,26 @@ form.addEventListener("submit", (e) => {
 // Function para converter a moeda
 function convertCurrency(amount, price, symbol){
   try {
+    //Exibindo a cotação da moeda
+    span.innerText = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    // Exibe total formatado
+    result.innerText = formatCurrencyBRL(amount * price).replace("R$", "") + " Reais"
+
+    // Aplica a classe que exibe o footer para mostrar o resultado
     footer.classList.add("show-result")
-    span.innerText = `${symbol} 1 = R$ ${price}`
-    result.innerText = amount * price
-
-
+    
   } catch (error) {
     console.log(error)
     footer.classList.remove("show-result")
     alert("Tente novamente!")
   }
+}
+
+// Formatação da moeda padrão
+function formatCurrencyBRL(value){
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
 }
